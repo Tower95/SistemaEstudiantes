5a5d9d9f790e9e897365acd573f6939a058af53c
Ismael Torres <torrestmi3@gmail.com>
2025-01-08 16:05:56 -0700

    testing connecion

--- src/main/java/xyz/Ismael/Connection/MyConnection.java
+++ src/main/java/xyz/Ismael/Connection/MyConnection.java
@@ -6,21 +6,28 @@ import java.sql.SQLException;
 
 public class MyConnection {
   public static Connection getConnection (){
+
     Connection  connection = null;
+
     var dataBase = "estudiantes_db";
     var url = "jdbc:mysql://localhost:3306/" + dataBase;
     var user = "root";
     var password = "example";
-    //Cargamos la clase del driver de mysql en memoria
 
+    // Load the class driver for Mysql in memory in execution time
     try {
       Class.forName("com.mysql.cj.jdbc.Driver");
       connection = DriverManager.getConnection(url, user, password);
 
     } catch (ClassNotFoundException | SQLException e){
-      System.out.println("Ocurrio un error en la conexion: " + e.getMessage());
+      System.out.println("And ERROR has occurre wiht Connection " + e.getMessage());
     }
     return connection;
   }
-
+  public static void main(String[] args){
+    var connection = MyConnection.getConnection();
+    if(connection != null){
+      System.out.println("SUCCESS Connection: " + connection);
+    }
+  }
 }
